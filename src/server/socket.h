f300f13bb4dd7a606f82d26ef01f02385773d17a
neptumiun <tymothe.billerey@gmail.com>
2024-04-22 13:05:12 +0000

    add guard for socket.h

--- src/server/socket.h
+++ src/server/socket.h
@@ -1,3 +1,5 @@
+#ifndef socket_H
+#define socket_H
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
@@ -6,3 +8,5 @@ void closeSocket (int *sockfd);
 int acceptClientConnetion (int *sockfd);
 void receiveDataFromClient (int s, char *buffer, int bufferSize);
 void sendDataToClient (int s, const char *buffer, int bufferSize);
+
+#endif /* end of include guard: socket_H */
