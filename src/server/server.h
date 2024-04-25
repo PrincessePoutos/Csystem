04908969b951df01ac83017395e95cf0eb7b0fe2
neptumiun <tymothe.billerey@gmail.com>
2024-04-25 06:49:10 +0000

    refactor(server): splite matchDomain

--- src/server/server.h
+++ src/server/server.h
@@ -1,14 +1,12 @@
 #ifndef server_H
 #define server_H
 
-#include <regex.h>
 #include <stdbool.h>
+
 #define BUFFER_SIZE 1024
 struct state
 {
   bool helo;
 };
-
-bool matchDomain (char *domain);
 
 #endif /* end of include guard: server_H */
